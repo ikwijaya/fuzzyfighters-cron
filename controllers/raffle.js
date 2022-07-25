88a56e00b2ca242250abdebc3658e83bbb147a1a
ikwijaya <irpan.kusuma@gmail.com>
2022-07-25 23:02:08 +0700

    add apikey headers

--- controllers/raffle.js
+++ controllers/raffle.js
@@ -5,7 +5,9 @@ const { API_URL, API_KEY, RAFFLE_ENDPOINT } = process.env
 const raffle = async () => {
     try {
         const endpoint = RAFFLE_ENDPOINT;
-        const response = await axios.default.get(API_URL+endpoint).catch(e => { throw (e) })
+        const headers = { apikey: API_KEY }
+        const response = await axios.default.get(API_URL+endpoint, { headers: headers })
+            .catch(e => { throw (e) });
 
         if(response.status != 200) return utils.failed(`Error when access API`, response.data);
         return utils.success(`success`, response.data)
